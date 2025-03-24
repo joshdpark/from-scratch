39fade055fb2c3745cee58d7069c2e3a93f95b19
Joshua Park <josh.d.park@gmail.com>
2025-03-24 14:57:47 -0400

    trying to make the code more elegant
    One way to make a loop more elegant is by reducing the number of if
    statements. The previous version used a while(true) : (i -= 1) and then had an if
    statement at the bottom to check if i == 0. The new version checks if i
    < 0, but decrements i at the top of the loop. This seems to be more
    elegant because while loops have this awkward pattern when traversing
    from the end of a slice to the front.

--- src/heapify.zig
+++ src/heapify.zig
@@ -4,22 +4,20 @@ fn heapify(array: []u32) void {
     // array[2 * parent + 1], and the right child is array[2 * parent + 2]
     //
     // identify the last-non leaf node
-    var i: u32 = @divFloor(@as(u32, @intCast(array.len)) - 1 - 1, 2);
-    while (true) : (i -= 1) {
-        var j = i;
-        while (true) {
-            const parent = j;
-            const left = 2 * j + 1;
-            if (left >= array.len) break;
-            const right = if (2 * j + 2 < array.len) 2 * j + 2 else left;
-            var max: u32 = parent;
+    var i: usize = @divFloor(array.len, 2);
+    while (i > 0) {
+        i -= 1;
+        var parent = i;
+        while (2 * parent + 1 < array.len) {
+            const left = 2 * parent + 1;
+            const right = if (left + 1 < array.len) left + 1 else left;
+            var max: usize = parent;
             max = if (array[max] > array[left]) max else left;
             max = if (array[max] > array[right]) max else right;
             if (max == parent) break;
             swap(&array[max], &array[parent]);
-            j = max;
+            parent = max;
         }
-        if (i == 0) break; // tree is balanced, need to break
     }
 }
 
@@ -31,11 +29,15 @@ fn swap(left: *u32, right: *u32) void {
 
 const std = @import("std");
 
+// todo: create a test suite that tests a huge slice of random values
+// against the std library implementation
+// perhaps a good time to try and use a fuzzer?
 test {
-    // first edge case: not a full tree, that is an odd len array
     var myarray: [5]u32 = .{ 4, 10, 100, 2, 200 };
     heapify(&myarray);
     try std.testing.expectEqualSlices(u32, &[5]u32{ 200, 10, 100, 2, 4 }, &myarray);
-    // todo: create a test suite that tests a huge slice of random values
-    // against the std library implementation
+    // first edge case: not a full tree, that is an odd len array
+    var b: [6]u32 = .{ 10, 5, 5, 1, 1, 6 };
+    heapify(&b);
+    try std.testing.expectEqualSlices(u32, &[6]u32{ 10, 5, 6, 1, 1, 5 }, &b);
 }
